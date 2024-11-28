6af6de2a7895f902fd543fba3afda518d1cb27f5
Yee Soon Tuck <yeesoontuck@gmail.com>
2024-11-28 13:56:47 +0800

    Q5 completed
    % modulus returns 0 if it is divisible

--- src/q5.py
+++ src/q5.py
@@ -5,10 +5,19 @@ def check_divisibility(num, divisor):
     - Both num and divisor must be numeric.
     - Return True if num is divisible by divisor, False otherwise.
     """
-    return
+
+    # modulus returns 0 if it is divisible
+    return (num % divisor) == 0
+    # return True
 
 
 # Task 2
 # Invoke the function "check_divisibility" using the following scenarios:
 # - 10, 2
 # - 7, 3
+
+print(check_divisibility(10, 2))
+print(check_divisibility(7, 3))
+
+# Reference
+# https://www.w3schools.com/python/python_operators.asp
