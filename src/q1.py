234151c76acf40e931fc46285ebf14102bf526b0
Yee Soon Tuck <yeesoontuck@gmail.com>
2024-11-28 12:42:29 +0800

    Q1 done
    Using
    isinstance()
    tuple
    if
    or
    else
    print

--- src/q1.py
+++ src/q1.py
@@ -6,10 +6,27 @@ def swap(x, y):
     - Return -1 if x and y is not numeric, and
     - print the swapped values if both x and y are numeric.
     """
-    return
+    
+    # numeric can be int or float, use tuple to test for both
+    if not(isinstance(x, (int, float))) or not(isinstance(y, (int, float))):
+        print(-1)
+    else:
+        x = x + y
+        y = x - y
+        x = x - y
+
+        print(x, y)
 
 
 # Task 2
 # Invoke the function "swap" using the following scenarios:
 # - "Apple", 10
 # - 9, 17
+# 
+swap("Apple", 10)
+swap(9, 17)
+
+# References
+# https://www.w3schools.com/python/python_conditions.asp
+# https://www.w3schools.com/python/ref_func_isinstance.asp
+# https://www.w3schools.com/python/python_operators.asp
