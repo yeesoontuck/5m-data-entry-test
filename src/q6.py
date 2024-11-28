2912d80fb8b8618f30a37a4facb589982c10beea
Yee Soon Tuck <yeesoontuck@gmail.com>
2024-11-28 14:05:05 +0800

    Q6 complete
    Using
    while loop to iterate the list
    +=  to increment index

--- src/q6.py
+++ src/q6.py
@@ -5,10 +5,23 @@ def find_first_negative(lst):
     - Return the first negative number if found, otherwise return "No negatives".
     - Use a while loop to implement this.
     """
-    return
+    
+    i = 0
+    while i < len(lst):
+        if lst[i] < 0:
+            print(lst[i])
+            return
+        i += 1
 
+    print("No negatives")
 
 # Task 2
 # Invoke the function "find_first_negative" using the following scenario:
 # - [3, 5, -1, 7, -2, 8]
 # - [2, 10, 7, 0]
+
+find_first_negative([3, 5, -1, 7, -2, 8])
+find_first_negative([2, 10, 7, 0])
+
+# Reference
+# https://www.w3schools.com/python/python_lists_loop.asp
