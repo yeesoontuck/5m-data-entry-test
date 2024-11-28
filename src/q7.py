ab94ccc1c401ca0900ddc89e0d0fa80fe8800ce6
Yee Soon Tuck <yeesoontuck@gmail.com>
2024-11-28 14:13:40 +0800

    Q7 completed
    Using
    class __init__
    print() with multiple objects, with default space separator

--- src/q7.py
+++ src/q7.py
@@ -6,10 +6,22 @@ class Car:
     - Add a method named describe_car() that prints information about the car as "Year Make Model"
     """
 
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
     def describe_car(self):
-        print("")
+        print(self.year, self.make, self.model)
 
 
 # Task 2
 # Create an instance of the Car class with the following attributes and call describe_car method:
 # - Make: Toyota, Model: Corolla, Year: 2020
+
+car = Car("Toyota", "Corolla", 2020)
+car.describe_car()
+
+# Reference
+# https://www.w3schools.com/python/python_classes.asp
+# https://www.w3schools.com/python/ref_func_print.asp
