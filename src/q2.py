34b08247f42d39cd86d65ffd4bd3bf8f7d7d54cc
Yee Soon Tuck <yeesoontuck@gmail.com>
2024-11-28 13:07:44 +0800

    Q2 done
    Using
    for
    list .append()

--- src/q2.py
+++ src/q2.py
@@ -5,10 +5,28 @@ def find_and_replace(lst, find_val, replace_val):
     - lst must be a list.
     - Return the modified list.
     """
-    return
 
+    if not(isinstance(lst, list)):
+        print("Input is not a list")
+        return
+    
+    newlist = []
+
+    for x in lst:
+        if x == find_val:
+            x = replace_val
+        newlist.append(x)
+    
+    print(newlist)
 
 # Task 2
 # Invoke the function "find_and_replace" using the following scenarios:
 # - [1, 2, 3, 4, 2, 2], 2, 5
 # - ["apple", "banana", "apple"], "apple", "orange"
+find_and_replace([1, 2, 3, 4, 2, 2], 2, 5)
+find_and_replace(["apple", "banana", "apple"], "apple", "orange")
+
+# References
+# https://www.w3schools.com/python/python_lists_comprehension.asp
+# https://www.w3schools.com/python/python_lists.asp
+# https://www.w3schools.com/python/python_lists_add.asp
