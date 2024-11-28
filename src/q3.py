7950058fc37ba6f15b6e439a59f3a72fc6c030c1
Yee Soon Tuck <yeesoontuck@gmail.com>
2024-11-28 13:19:52 +0800

    Q3 completed
    Using
    if key in dictionary
    string concatenation
    cast number to string for concatenation

--- src/q3.py
+++ src/q3.py
@@ -5,10 +5,24 @@ def update_dictionary(dct, key, value):
     - If the key already exists in dct, print the original value, then update its value.
     - Return the updated dictionary.
     """
-    return
+
+    if key in dct:
+        print("Original value of " + key + " is: " + str(dct[key]))
+
+    dct[key] = value
+
+    print(dct)
 
 
 # Task 2
 # Invoke the function "update_dictionary" using the following scenarios:
 # - {}, "name", "Alice"
 # - {"age": 25}, "age", 26
+
+update_dictionary({}, "name", "Alice")
+update_dictionary({"age": 25}, "age", 26)
+
+# References
+# https://www.w3schools.com/python/python_dictionaries_access.asp
+# https://www.w3schools.com/python/python_casting.asp
+# https://www.w3schools.com/python/python_strings_concatenate.asp
